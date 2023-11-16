7f8afebf111882eda74ca44112f313ca8e0b6c09
suraj patil <3surajpatil@gmail.com>
2023-11-16 13:19:02 +0530

    updates Nov 16-2023

---
+++ mysql-commands.sql
@@ -0,0 +1,34 @@
+
+
+select * from employee;
+-- select * from hibernate_sequence;
+
+-- ALTER TABLE employee add(bonous float(5,2));
+
+-- delete from employee where bonous is null;
+-- delete from employee where ename is null;
+-- delete  from employee;
+
+commit;
+
+  SELECT * FROM nifty200 WHERE TRADE_DATE='2023-09-13';
+  -- delete from nifty200 WHERE TRADE_DATE='2023-09-13';
+
+select * from nifty200 limit 1;
+select * from nifty200 where symbol='TECHM' AND trade_date='2023-08-21' LIMIT 1;
+select distinct(SYMBOL) FROM nifty200;
+
+select id,symbol,trade_date,open,high,low,ltp as close FROM nifty200 
+where symbol='NAUKRI' AND trade_date between '2023-07-17' and '2023-07-21' 
+order by trade_date desc;
+
+
+
+
+select count(*) from nifty200;
+
+
+
+
+
+
